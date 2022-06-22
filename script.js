fb1532832d21630cebbf2a6a980499ff7b67a84e
Eric Castro <eric-henrique44@hotmail.com>
2022-06-21 22:44:19 -0300

    Adicionando Backgrund Animado

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login-button');
+const checkboxWallpaper = document.getElementById('checkboxWallpaper');
+const wallpaper = document.getElementById('wallpaper');
+const walppaperAnimation = 'wallpaper-animation';
+
+function animationWallpaper(){
+  wallpaper.classList.toggle(walppaperAnimation)
+  console.log(wallpaper);
+}
+
 
 const handleFocus = ({ target }) => {
   const span = target.previousElementSibling;
@@ -26,3 +35,5 @@ const handleChange = () => {
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 inputs.forEach((input) => input.addEventListener('input', handleChange));
+
+checkboxWallpaper.addEventListener('click', animationWallpaper);
